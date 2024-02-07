938ac478c24d6600647e0e880ed73a95530d2250
Rodrigo <fraurcino@hotmail.com>
2024-02-07 11:55:05 -0600

    fix: rename endpoint and new modules

--- wordle/main.py
+++ wordle/main.py
@@ -1,13 +1,9 @@
 from typing import Union
 
 from fastapi import FastAPI
-from wordle.infra.rest.endpoints import router as user_router
+from wordle.infra.rest.users.endpoints import router as user_router
 
 app = FastAPI()
 
 app.include_router(user_router)
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
