af09b0a240c7299da567dfee84c2e8d0871311f8
Rodrigo <fraurcino@hotmail.com>
2024-02-07 21:12:27 -0600

    feat: add redis cache for validae attempts

--- wordle/infra/rest/wordle/endpoints.py
+++ wordle/infra/rest/wordle/endpoints.py
@@ -4,19 +4,22 @@ from sqlalchemy.orm import Session
 from common.infra.auth.token import get_current_user
 from common.infra.database import get_db
 from wordle.app.wordle_play import WordlePlayProcessor
+from common.infra.cache import redis_client
 from wordle.infra.repositories.word_repository import PostgresWordRepository
+from wordle.infra.repositories.cache_repository import RedisCacheRepository
 from wordle.infra.models.wordle import WordleRequest
 
 router = APIRouter()
 
 
-
 @router.post('/wordle/')
 def wordle_play(word: WordleRequest, db: Session = Depends(get_db), current_user: dict = Depends(get_current_user)):
     postgres_word_repository = PostgresWordRepository(db=db)
+    redis_cache_repository = RedisCacheRepository(redis_client)
     result = WordlePlayProcessor(
         word_repository=postgres_word_repository,
         word=word.word,
+        cache_repository=redis_cache_repository,
         user=current_user,
     ).execute()
     return result
