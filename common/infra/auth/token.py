4ade2a0da2c92c93e7dcbcedb75c77f118ee89bd
Rodrigo <fraurcino@hotmail.com>
2024-02-08 17:53:54 -0600

    fix: update secret key

--- common/infra/auth/token.py
+++ common/infra/auth/token.py
@@ -5,7 +5,7 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
 
-SECRET_KEY = "mysecretkey"
+SECRET_KEY = "jqELpKJAAFWVT"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
