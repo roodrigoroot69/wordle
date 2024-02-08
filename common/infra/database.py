1cf4405e37bc63a6966bbe7407b20072234b720b
Rodrigo <fraurcino@hotmail.com>
2024-02-08 17:48:32 -0600

    feat: update database and readme

--- common/infra/database.py
+++ common/infra/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
 
 
-DATABASE_URL = "postgresql://rjbnuehw:Y1qArOnlvJp_ht_H8lBLbbcsMsCAQHI8@baasu.db.elephantsql.com/rjbnuehw"
+DATABASE_URL = "postgresql://doadmin:AVNS_VwfCPQdLzfutDziTaU-@dd360-do-user-15693400-0.c.db.ondigitalocean.com:25060/defaultdb"
 engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(bind=engine)
 
