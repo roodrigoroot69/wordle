5f15a94aed5b45cf91d2af98601ad994d1366f87
Rodrigo <fraurcino@hotmail.com>
2024-02-08 03:24:52 -0600

    fix: clean cache each 5 minutes

--- wordle/domain/utils/change_word.py
+++ wordle/domain/utils/change_word.py
@@ -2,14 +2,18 @@ import threading
 
 from common.infra.database import SessionLocal
 from common.infra.models import Words
+from common.infra.cache import redis_client
+
 from sqlalchemy.sql.expression import and_, func
 
 
 db = SessionLocal()
 
 
+
 def desactive_word():
     word = db.query(Words).filter(and_(func.length(Words.word) == 5, Words.is_active == True)).first()
     word.is_active = False
     db.commit()
+    redis_client.flushall()
     threading.Timer(300, desactive_word).start()
